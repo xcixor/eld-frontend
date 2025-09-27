aa33b203afa3e53bf6ec74676026dbbb39558a2a
xcixor <pndungu54@gmail.com>
2025-09-27 08:56:36 +0300

    add turbo config settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+const path = require('path')
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  },
 };
 
 export default nextConfig;
