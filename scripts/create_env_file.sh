d398f9f1ccaf6d948aab7ddbdceab6002f9426c9
xcixor <pndungu54@gmail.com>
2025-09-29 12:53:53 +0300

    fix: correct environment variable name for base API URL in create_env_file script

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -4,7 +4,7 @@ create_env_file() {
     touch .env
     echo "NEXTAUTH_URL=${NEXTAUTH_URL}" >> .env
     echo "NEXTAUTH_SECRET=${NEXTAUTH_SECRET}" >> .env
-    echo "NEXT_PUBLIC_ELD_API_URL=${NEXT_PUBLIC_ELD_API_URL}" >> .env
+    echo "NEXT_PUBLIC_BASE_API_URL=${NEXT_PUBLIC_BASE_API_URL}" >> .env
 }
 main(){
     create_env_file
